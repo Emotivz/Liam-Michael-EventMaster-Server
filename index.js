9424016adc75cf3cb02b7e8a0f786e448adb58b3
Michael Orimolade <siwsher@hotmail.com>
2023-05-31 20:39:47 +0100

    initial commit & folder structure

---
+++ index.js
@@ -0,0 +1,16 @@
+//importing dependencies
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const eventRoutes = require("./routes/eventRoutes");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json);
+
+app.use("/events", eventRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server had started on port ${PORT}`);
+});
---
+++ routes/eventRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const eventsData = require("../data/events.json");
+
+//define routes
+
+module.exports = router;
