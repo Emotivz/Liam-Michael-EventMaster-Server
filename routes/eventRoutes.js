d278e54e6196d4e59bf3e86797992e4f0710a790
Michael Orimolade <siwsher@hotmail.com>
2023-06-01 12:35:42 +0100

    add short event list route for homepage

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,6 +2,22 @@ const express = require("express");
 const router = express.Router();
 const eventsData = require("../data/events.json");
 
-//define routes
+//routes
+
+// GET list of events
+// This route can be used for home page
+//rename eventd. Can't use event as this is a keyword
+router.get("/", (req, res) => {
+  const shortEventListData = eventsData.map((event) => {
+    return {
+      id: event.id,
+      title: event.title,
+      date: event.date,
+      host: event.host,
+      image: event.image,
+    };
+  });
+  res.status(200).json(shortEventListData);
+});
 
 module.exports = router;
